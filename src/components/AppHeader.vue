0053cee44eed496c195ea38395bf81470a4e08d8
Bruno Canini <brunocanini1994@gmail.com>
2023-05-20 16:56:01 +0200

    array

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,21 @@
+<script>
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            links : [ "CHARACTERS" , "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP" ]
+        }
+    }
+}
+</script>
+
 <template>
     <div class="containerHeader">
         <div class="logo">
             <img src="/public/dc-logo.png" alt="">
         </div>
         <div class="nav">
-            <a href="">CHARACTERS</a>
-            <a href="">COMICS</a>
-            <a href="">MOVIES</a>
-            <a href="">TV</a>
-            <a href="">GAMES</a>
-            <a href="">COLLECTIBLES</a>
-            <a href="">VIDEOS</a>
-            <a href="">FANS</a>
-            <a href="">NEWS</a>
-            <a href="">SHOP</a>
+            <a v-for="link in links" href="">{{link}}</a>
         </div>
     </div>
 </template>
