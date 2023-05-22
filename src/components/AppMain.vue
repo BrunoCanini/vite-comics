927c3331b915065ca18e673997536e94118e4751
Bruno Canini <brunocanini1994@gmail.com>
2023-05-22 17:42:51 +0200

    scheletro slides

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,20 +1,40 @@
+<script>
+export default {
+    name: "appMain",
+    props: [
+        "slides"
+    ]
+}
+</script>
+
 <template>
-    <div class="containerMain">
-        <h1>Content goes here</h1>
+    <div class="containerSlaider"></div>
+    <div class="containerBox">
+        <div v-for="slide in slides" class="box">
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .containerMain{
+    .containerSlaider{
         background-color: black;
-        height: 135px;
-        padding: 0 310px;
-        display: flex;
-        align-items: center;
+        height: 400px;
     }
 
-    h1{
-        color: white;
+    .containerBox{
+        height: 670px;
+        padding: 30px 310px;
+        background-color: bisque;
+        display: flex;
+        flex-wrap: wrap;
     }
+
+    .box{
+        margin: 10px;
+        width: calc(100% / 6 - 20px);
+        height: 245px;
+        border: 1px solid black;
+    }
+
 
 </style>
