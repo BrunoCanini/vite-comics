73470aa8e60290b9581e211791094c180d0bdc99
Bruno Canini <brunocanini1994@gmail.com>
2023-05-22 18:08:14 +0200

    slide

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,11 @@
 <script>
 export default {
     name: "appMain",
-    props: [
-        "slides"
-    ]
+    props: {
+        slides: Array
+    }
+       
+    
 }
 </script>
 
@@ -11,6 +13,12 @@ export default {
     <div class="containerSlaider"></div>
     <div class="containerBox">
         <div v-for="slide in slides" class="box">
+            <div class="boxImg">
+                <img :src=" slide.thumb" alt="">
+            </div>
+            <div class="boxTitle">
+                <h3> {{ slide.series }}</h3>
+            </div>
         </div>
     </div>
 </template>
@@ -22,9 +30,8 @@ export default {
     }
 
     .containerBox{
-        height: 670px;
         padding: 30px 310px;
-        background-color: bisque;
+        background-color: #1c1c1c;
         display: flex;
         flex-wrap: wrap;
     }
@@ -32,8 +39,22 @@ export default {
     .box{
         margin: 10px;
         width: calc(100% / 6 - 20px);
-        height: 245px;
-        border: 1px solid black;
+        max-width: 195px;
+    }
+
+    .boxImg{
+        width: 100%;
+        height: 200px;
+    }
+
+    img{
+        width: 100%;
+        height: 100%;
+    }
+
+    h3{
+        padding-top: 1rem;
+        color: white;
     }
 
 
